5ea17059413d3462679a6cf8f799be9f49fcea35
hyunwlee-dev <hwlee3928@gmail.com>
2023-08-27 22:56:12 +0900

    stlye: add normalize, reset css add: color variables

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,11 @@
-import Link from 'next/link';
-import styled from 'styled-components';
-import Button from '@/components/Button';
-import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import BaseLayout from '@/components/Layout';
-import Slider from '@/components/Slider';
-import MainHome from '@/pages/MainHome';
-
-const StyledDiv = styled.div`
-  background-color: #fff;
-  max-width: 1200px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const StyledLink = styled.a`
-  color: #ff5500;
-  text-decoration: none;
-  font-weight: bold;
-  font-size: 24px;
-`;
+import {MainHomeContainer} from '@/containers/MainHomeContainer';
 
 export default function Home() {
   return (
-    <BaseLayout>
-      <Slider />
-      <MainHome />
-      {/* <h1 style={{color: '#FF5500'}}>Welcome to MofMof Website!</h1>
-      <Link href="/FattailGecko" legacyBehavior>
-        <StyledLink>펫테일</StyledLink>
-      </Link> */}
-    </BaseLayout>
+    <>
+      <MainHomeContainer />
+    </>
   );
 }
