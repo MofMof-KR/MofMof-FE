c66a7b1b68ee8f0de91ce59ccfbd7c73d991c9f6
hyunwlee-dev <hwlee3928@gmail.com>
2023-08-27 23:03:44 +0900

    feat: MainHomePage 마크업

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,46 @@
-import Footer from '@/components/Footer';
+import Container from '@/components/Container';
+import {DefaultHeader} from '@/components/Header';
 import BaseLayout from '@/components/Layout';
+import Logo from '@/components/Logo';
 import {MainHomeContainer} from '@/containers/MainHomeContainer';
+import Image from 'next/image';
+import Footer from '@/components/Footer';
+import * as S from './Home.style';
 
 export default function Home() {
-  return (
+  const rightSide = (
     <>
-      <MainHomeContainer />
+      <S.StyledButton>
+        <Image
+          src={'/images/home/Chat.svg'}
+          width={40}
+          height={40}
+          alt={'notification'}
+        />
+      </S.StyledButton>
+      <S.StyledButton>
+        <Image
+          src={'/images/home/nNotifications.svg'}
+          width={40}
+          height={40}
+          alt={'notification'}
+        />
+      </S.StyledButton>
     </>
+  );
+  const header = (
+    <DefaultHeader
+      headText="mofmof"
+      isHeadShown={false}
+      leftSide={<Logo />}
+      rightSide={rightSide}
+    />
+  );
+  return (
+    <BaseLayout headerSide={header} isNavShown footerSide={<Footer />}>
+      <Container>
+        <MainHomeContainer />
+      </Container>
+    </BaseLayout>
   );
 }
