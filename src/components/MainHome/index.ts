4574398a7153bec4c73bbe05d1c3cf47922f23cf
hyunwlee-dev <hwlee3928@gmail.com>
2023-08-27 19:32:25 +0900

    feat #23: MainHomeContainer in MainHomePage

---
+++ src/components/MainHome/index.ts
@@ -0,0 +1,6 @@
+export {SearchForm} from './SearchForm';
+export {AllAboutMof} from './AllAboutMof';
+export {MofCommunity} from './MofCommunity';
+export {RecentTopSearches} from './RecentTopSearches';
+export {PopularArticles} from './PopularArticles';
+export {FAQ} from './FAQ';
