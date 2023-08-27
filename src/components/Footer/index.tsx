435f41d96fc8f55c4b0969e30898bb04db6acff9
hyunwlee-dev <hwlee3928@gmail.com>
2023-08-27 23:01:19 +0900

    fix: Footer 디자인 수정

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,52 +1,36 @@
 import {HTMLAttributes} from 'react';
-import {StyledFooter, InfoWrapper, SnsWrapper} from './style';
 import Image from 'next/image';
 import Link from 'next/link';
+import * as S from './Footer.style';
 
-interface IProps extends HTMLAttributes<HTMLDivElement> {
-  className?: string;
-}
-const Footer: React.FC<IProps> = ({className = '', ...restProps}) => {
+type IProps = HTMLAttributes<HTMLDivElement>;
+
+const socialObjs = [
+  {name: 'youtube', link: '#'},
+  {name: 'facebook', link: '#'},
+  {name: 'twitter', link: '#'},
+  {name: 'instagram', link: '#'},
+  {name: 'linkedin', link: '#'},
+];
+
+const Footer: React.FC<IProps> = ({...restProps}) => {
+  const year = new Date().getFullYear();
   return (
-    <StyledFooter className={className} {...restProps}>
-      <p className="bold">MofMof Inc.</p>
-      <InfoWrapper>
-        <p>대표 : 함종석</p>
-        <p>사업자 등록번호 : 001-01-00001</p>
-      </InfoWrapper>
-      <InfoWrapper>
-        <p>대표번호 : +82)1-234-5678</p>
-        <p>홈페이지 : mofmof.com</p>
-      </InfoWrapper>
-      <p className="bold">COPYRIGHT ALL RESERVED. 2023 © MofMof-KR</p>
-      <SnsWrapper>
-        <Link href="https://www.instagram.com/" legacyBehavior>
-          <a target="_blank" rel="noopener noreferrer">
+    <S.Footer {...restProps}>
+      <p>{`MofMof @ ${year}. All rights reserved.`}</p>
+      <S.SocialLinks>
+        {socialObjs?.map(({name, link}) => (
+          <Link href={link} key={name}>
             <Image
-              src={'/instagram.svg'}
-              alt="인스타그램"
-              width={30}
-              height={30}
+              src={`/images/nav/${name}.svg`}
+              width={23}
+              height={23}
+              alt={name}
             />
-          </a>
-        </Link>
-        <Link href="https://www.facebook.com/" legacyBehavior>
-          <a target="_blank" rel="noopener noreferrer">
-            <Image
-              src={'/facebook.svg'}
-              alt="페이스북"
-              width={30}
-              height={30}
-            />
-          </a>
-        </Link>
-        <Link href="https://www.youtube.com/" legacyBehavior>
-          <a target="_blank" rel="noopener noreferrer">
-            <Image src={'/youtube.svg'} alt="유튜브" width={30} height={30} />
-          </a>
-        </Link>
-      </SnsWrapper>
-    </StyledFooter>
+          </Link>
+        ))}
+      </S.SocialLinks>
+    </S.Footer>
   );
 };
 
