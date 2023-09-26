4803c31417749bb60ab4fde6bede18f224b008e3
hyunwlee-dev <hwlee3928@gmail.com>
2023-09-27 01:47:10 +0900

    chore: redirect from fattail to home

--- src/pages/fattail/index.tsx
+++ src/pages/fattail/index.tsx
@@ -5,22 +5,28 @@ import BaseLayout from '@/components/Layout';
 import * as S from './Fattail.style';
 import Image from 'next/image';
 import FattailContainer from '@/containers/FattailContainer';
-
-const leftSide = (
-  <S.GoBackButton>
-    <Image
-      src={'/images/home/iconamoon_arrow-up-2.svg'}
-      width={24}
-      height={24}
-      alt={'go back'}
-    />
-  </S.GoBackButton>
-);
-const header = (
-  <DefaultHeader headText="펫테일 게코" isHeadShown leftSide={leftSide} />
-);
+import {useRouter} from 'next/router';
 
 const Fattail = () => {
+  const router = useRouter();
+  const leftSide = (
+    <S.GoBackButton
+      onClick={() => {
+        router.back();
+      }}
+    >
+      <Image
+        src={'/images/home/iconamoon_arrow-up-2.svg'}
+        width={24}
+        height={24}
+        alt={'go back'}
+      />
+    </S.GoBackButton>
+  );
+  const header = (
+    <DefaultHeader headText="펫테일 게코" isHeadShown leftSide={leftSide} />
+  );
+
   return (
     <BaseLayout headerSide={header} isNavShown footerSide={<Footer />}>
       <Container>
